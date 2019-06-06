f05469edf65330b72814453faf3915d9e7e65857
MariusA <anca.ticau@gmail.com>
2019-06-06 22:51:11 +0300

    Exercise 4

---
+++ Strings-Ex4/Strings-Ex4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Strings_Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = ReverseString("This is a string");
+            Console.WriteLine(test);
+
+            test = ReverseString("This is a stringgg");
+            Console.WriteLine(test);
+        }
+
+        //Write a method that reverses a string if it's length is a multiple of 4
+
+        public static string ReverseString(string input)
+        {
+            if (input.Length % 4 != 0)
+            {
+                return input;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = input.Length - 1; j >= 0; j--)
+            {
+                sb.Append(input[j]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
